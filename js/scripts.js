a31876ec258075b28cca262cd4b63f0aa12986cb
Laura Hope <ssteamlaura@gmail.com>
2023-04-05 15:20:35 -0700

    Add UI functionality for form submission, button interactivity, and displaying game details.
    Co-authored-by: Gareth Grindeland <gt.grindeland@gmail.com>
    Co-authored-by: Joseph Wahbeh <Whobbes22@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,121 @@
-  //Business Logic for Players
-  
-  function Player(playerName) {
-    this.playerName = playerName;
-    this.totalScore = 0;
+//Buisness Logic for Game
+
+function Game(){
+  this.gamePlayers = {}
+  this.currentId = 0;
+}
+
+Game.prototype.addPlayer = function(player){
+  player.id = this.assignId();
+  this.gamePlayers[player.id] = player
+};
+
+Game.prototype.assignId = function(){
+  this.currentId += 1;
+  return this.currentId;
+};
+
+Game.prototype.findPlayer = function (id){
+  if(this.gamePlayers[id] !== undefined){
+    return this.addPlayer[id];
+  } else {
+    return this.addPlayer[0];
+  }
+};
+
+//Business Logic for Players
+function Player(playerName) {
+  this.playerName = playerName;
+  this.totalScore = 0;  
+  this.roundScore = 0;
+  this.roll = 0;
+  //this.scoreHistory = [];
+}
+
+//Business Logic for Player Turns
+
+Player.prototype.playerTurn = function() {
+  this.rollDice();
+  console.log(this.roll,this.roundScore);
+  if (this.roll === 1) {
     this.roundScore = 0;
-    this.roll = 0;
-    //this.scoreHistory = [];
+    this.passTurn();
+    console.log("roll = 0, pass turn");
+  } else  {
+    this.roundScore += this.roll;
   }
-
-  //Business Logic for Player Turns
-
-  Player.prototype.playerTurn = function() {
-    this.rollDice();
-    console.log(this.roll,this.roundScore);
-    if (this.roll === 1) {
-      this.roundScore = 0;
-      this.passTurn();
-      console.log("roll = 0, pass turn");
-    } else  {
-      this.roundScore += this.roll;
-    }
-    this.checkWin();
-    
-    // call function to show updated score
-  }
-
-  Player.prototype.rollDice = function() {
-    this.roll = Math.ceil(Math.random()*6); 
-  };
-
-  Player.prototype.checkWin = function(){
-    if(this.roundScore + this.totalScore >= 100){
-      alert("This player has won the game");
-    }
-  };
-
-  Player.prototype.holdDice = function(){
-    this.totalScore += this.roundScore;
-    this.roundScore = 0;
-  };
-
-  Player.prototype.passTurn = function() {
-   // update database of palyers to go to next player in the list 
-  }
-
-  //UI Logic  
-  function displayPlayers(player)  {
-    // const player = // get input from data base
-    document.querySelector("#player-name").innerText = player.playerName;
-    document.querySelector("#roll-amount").innerText = player.roll;
-    document.querySelector("#round-amount").innerText = player.roundScore;
-    document.querySelector("#total-score").innerText = player.totalScore;
-  }
-
-  let newPlayer; // global variable to be removed
+  this.checkWin();
   
-  function handleFormSubmission(event)  {
-    event.preventDefault();
-    const inputtedPlayerName = document.querySelector("input#name1").value;
-    newPlayer = new Player(inputtedPlayerName);
-    console.log("the new player constructor: ",newPlayer)
-    // add newPlayer to game here if we make a Game() constructor
-    displayPlayers(newPlayer); // change to display game later
-    document.querySelector("input#name1").value = null;
+  // call function to show updated score
+};
+
+Player.prototype.rollDice = function() {
+  this.roll = Math.ceil(Math.random()*6); 
+};
+
+Player.prototype.checkWin = function(){
+  if(this.roundScore + this.totalScore >= 100){
+    alert("This player has won the game");
+  }
+};
+
+Player.prototype.holdDice = function(){
+  this.totalScore += this.roundScore;
+  this.roundScore = 0;
+};
+
+Player.prototype.passTurn = function() {
+  // WIP
+  // update database of palyers to go to next player in the list 
+};
+
+//UI Logic
+//Global Variable for Game - database
+let game = new Game();
+
+function displayGame(gameToDisplay)  {
+
+  Object.keys(gameToDisplay.gamePlayers).forEach(function(key) {
+  
   }
 
-  function handleRollButton(event)  {
-    event.preventDefault();
-    newPlayer.playerTurn();
-    displayPlayers(newPlayer); // change to display game later
-  }
+  const currentPlayer = gameToDisplay.findPlayer(player.id); // players- get input from data base correctly
+  document.querySelector("#player-name").innerText = currentPlayer.playerName;
+  document.querySelector("#roll-amount").innerText = currentPlayer.roll;
+  document.querySelector("#round-amount").innerText = currentPlayer.roundScore;
+  document.querySelector("#total-score").innerText = currentPlayer.totalScore;
 
-  function handlePassButton(event)  {
-    event.preventDefault();
-    newPlayer.holdDice();
-    displayPlayers(newPlayer); // change to display game later
-  }
-    
-  window.addEventListener("load", function(){
-    document.querySelector("form#game-setup").addEventListener("submit", handleFormSubmission);
-    document.querySelector("button#roll").addEventListener("click", handleRollButton);
-    document.querySelector("button#pass").addEventListener("click", handlePassButton);
-  });
+  //   document.querySelector("#everyone-score").innerText = player.totalScore;
+}
+
+//let newPlayer; // global variable to be removed
+
+function handleFormSubmission(event)  {
+  event.preventDefault();
+  const inputtedPlayerName = document.querySelector("input#name1").value;
+  let newPlayer = new Player(inputtedPlayerName);
+  console.log("the new player constructor: ",newPlayer)
+  game.addPlayer(newPlayer);
+  // add newPlayer to game here if we make a Game() constructor
+  displayGame(game); 
+  document.querySelector("input#name1").value = null;
+}
+
+function handleRollButton(event)  {
+  //Need to find which id to grab
+  event.preventDefault();
+  newPlayer.playerTurn();
+  displayPlayers(newPlayer); // change to display game later
+}
+
+function handlePassButton(event)  {
+  event.preventDefault();
+  newPlayer.holdDice();
+  displayPlayers(newPlayer); // change to display game later
+}
+  
+window.addEventListener("load", function(){
+  document.querySelector("form#game-setup").addEventListener("submit", handleFormSubmission);
+  document.querySelector("button#roll").addEventListener("click", handleRollButton);
+  document.querySelector("button#hold").addEventListener("click", handlePassButton);
+});
