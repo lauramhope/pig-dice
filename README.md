c5db66a42ed2eb15cf0d53d7900e5bac455eae73
Laura Hope <ssteamlaura@gmail.com>
2023-04-05 13:16:33 -0700

    Add HTML template with forms, divs, inputs, labels, inputs.

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-Describe: rollDice()
+Describe: Player()
+
+Test: "It will generate a player for the game starting with total score of 0 and round score of 0"
+Code: Player();
+Expected Output: Player.totalScore = 0, Player.roundScore = 0;
+
+Describe: Player.prototype.rollDice()
 
 Test: "It will roll dice, generating a number between 1-6"
-Code: 
-function rollDice(min, max) {
-  return Math.ceil(Math.random()*6);
-}
-Input: rollDice(1,6)
-Expected Output: random positive integer
+Code: rollDice();
+Expected Output: random positive integer between 1 and 6
 
-Test:
-Code:
-Input:
-Expected Output:
+Describe: playerTurn()
+
+Test: "It will take each player's turn rolling the dice and include branching depending on what the player rolls"
+Code: playerTurn();
+Expected Output: random dice number between 1-6
+
+Describe: Player.prototype.checkWin()
+
+Test: "It will check to see if the sum of the current round score and the total score is greater than or equal to 100"
+Code: checkWin();
+Expected Output: if total score >= 100 , player has won
+
+Describe: Player.prototype.holdDice()
+
+Test: "It will hold the dice on current player's turn and give the sum of the round score and total score."
+Code: holdDice()
+Expected Output: 
+
+Describe: Player.prototype.passTurn()
+
+Test: 
+Code: 
+Expected Output: 
+
+
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,78 @@
-Math.ceil(Math.random()*6);
+  //Business Logic for Players
+  
+  function Player(playerName) {
+    this.playerName = playerName;
+    this.totalScore = 0;
+    this.roundScore = 0;
+    this.roll = 0;
+    //this.scoreHistory = [];
+  }
+
+  //Business Logic for Player Turns
+
+  Player.prototype.playerTurn = function() {
+    this.rollDice();
+    console.log(this.roll,this.roundScore);
+    if (this.roll === 1) {
+      this.roundScore = 0;
+      this.passTurn();
+      console.log("roll = 0, pass turn");
+    } else  {
+      this.roundScore += this.roll;
+    }
+    this.checkWin();
+    
+    // call function to show updated score
+  }
+
+  Player.prototype.rollDice = function() {
+    this.roll = Math.ceil(Math.random()*6); 
+  };
+
+  Player.prototype.checkWin = function(){
+    if(this.roundScore + this.totalScore >= 100){
+      alert("This player has won the game");
+    }
+  };
+
+  Player.prototype.holdDice = function(){
+    this.totalScore += this.roundScore;
+    this.roundScore = 0;
+  };
+
+  Player.prototype.passTurn = function() {
+   // update database of palyers to go to next player in the list 
+  }
+
+  //UI Logic  
+  function displayPlayers(player)  {
+    // const player = // get input from data base
+    document.querySelector("#player-name").innerText = player.playerName;
+    document.querySelector("#roll-amount").innerText = player.roll;
+    document.querySelector("#round-amount").innerText = player.roundScore;
+    document.querySelector("#total-score").innerText = player.totalScore;
+  }
+
+  function handleFormSubmission(event)  {
+    event.preventDefault();
+    const inputtedPlayerName = document.querySelector("input#name1").value;
+    let newPlayer = new Player(inputtedPlayerName);
+    // add newPlayer to game here if we make a Game() constructor
+    displayPlayers(newPlayer); // change to display game later
+    document.querySelector("input#name1").value = null;
+  }
+
+  function handleRollButton(event)  {
+    event.preventDefault();
+    // player1.playerTurn();
+  }
+
+  function handlePassButton(event)  {
+    event.preventDefault();
+  }
+    
+  window.addEventListener("load", function(){
+    document.querySelector("form#game-setup").addEventListener("submit", handleFormSubmission);
+    document.querySelector("button#roll").addEventListener("click", handleRollButton);
+    document.querySelector("button#pass").addEventListener("click", handlePassButton);
+  });
