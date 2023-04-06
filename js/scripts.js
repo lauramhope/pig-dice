767c2d953ab2578079050d20b8d426f459470769
Laura Hope <ssteamlaura@gmail.com>
2023-04-06 13:21:35 -0700

    Fix bug: now able to add multiple players and switch between them with rolling dice and holding dice.
    Co-authored by: Joseph Wahbeh <Whobbes22@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -23,16 +23,15 @@ Game.prototype.findPlayer = function(){
   // } else {
   //   return this.addPlayer[0];
   // }
-  let curId = this.currentId;
-  let gp = this.gamePlayers;
-  Object.keys(gp).forEach(function(key) {
-   // console.log("key",key,this.currentId,this.currentId.toString() ===  key)
-    if(curId.toString() ===  key){
-      return gp[key];
-      //currentPlayer.playerTurn();
+  let currentPlayer;
+  Object.keys(this.gamePlayers).forEach(function(key) {
+    //  console.log("key",key,typeof(gameToDisplay.currentId),gameToDisplay.currentId.toString() ===  key)
+    if(game.currentId.toString() ===  key){
+      currentPlayer = this.gamePlayers[key];
     }
   });
-};
+  return currentPlayer;
+}
 
 Game.prototype.updateCurrentId = function(){
   console.log("type current + max:",typeof(this.currentId),typeof(this.maxId));
@@ -57,14 +56,15 @@ function Player(playerName) {
 Player.prototype.playerTurn = function() {
   this.rollDice();
   console.log(this.roll,this.roundScore);
-  if (this.roll === 1) {
-    this.roundScore = 0;
-    this.holdDice();
-    console.log("roll = 0, pass turn");
-  } else  {
-    this.roundScore += this.roll;
-  }
-  this.checkWin();
+  // if (this.roll === 1) {
+  //   this.roundScore = 0;
+  //   this.holdDice();
+  //   console.log("roll = 0, pass turn");
+  // } else  {
+  //   this.roundScore += this.roll;
+  // }
+  this.roundScore += this.roll;
+ // this.checkWin();
   
   // call function to show updated score
 };
@@ -82,7 +82,7 @@ Player.prototype.checkWin = function(){
 Player.prototype.holdDice = function(){
   this.totalScore += this.roundScore;
   this.roundScore = 0;
-  game.updateCurrentId();
+  this.roll = 0;
 };
 
 // Player.prototype.passTurn = function() {
@@ -95,12 +95,10 @@ Player.prototype.holdDice = function(){
 //Global Variable for Game - database
 let game = new Game();
 
-
-
 function displayGame(gameToDisplay, branch)  {
   let currentPlayer = gameToDisplay.gamePlayers[1];
   console.log(currentPlayer);
-  let curId= gameToDisplay.currentId;
+  let rolledOne = false;
   switch(branch){
     case(1): 
       // if(gameToDisplay.maxId === 1){
@@ -115,6 +113,12 @@ function displayGame(gameToDisplay, branch)  {
           currentPlayer.playerTurn();
         }
       });
+      if(currentPlayer.roll === 1){
+        rolledOne = true;
+        gameToDisplay.updateCurrentId();
+        currentPlayer = gameToDisplay.gamePlayers[gameToDisplay.currentId];
+      }
+      currentPlayer.checkWin();
       break;
     case(3):
     Object.keys(gameToDisplay.gamePlayers).forEach(function(key) {
@@ -124,18 +128,35 @@ function displayGame(gameToDisplay, branch)  {
         currentPlayer.holdDice();
       }
     });
+    gameToDisplay.updateCurrentId();
+    currentPlayer = gameToDisplay.gamePlayers[gameToDisplay.currentId];
     break;
 }
-    changeInfoPlayer(currentPlayer);
+    changeInfoPlayer(currentPlayer,rolledOne);
 
 }
 
-function changeInfoPlayer(currentPlayer){
+function changeInfoPlayer(currentPlayer,rolledOne){
+  if(rolledOne){
+    document.querySelector("#rolled-one").innerText = "Sorry the previous player rolled a one and it is not there turn anymore!";
+  } else{
+    document.querySelector("#rolled-one").innerText = "";
+  }
   document.querySelector("#player-name").innerText = currentPlayer.playerName;
   document.querySelector("#roll-amount").innerText = currentPlayer.roll;
   document.querySelector("#round-amount").innerText = currentPlayer.roundScore;
-  document.querySelector("#total-score").innerText = currentPlayer.totalScore;
+  document.querySelector("#total-score").innerText = currentPlayer.totalScore;  
   //   document.querySelector("#everyone-score").innerText = player.totalScore;
+}
+
+function updateTotalPlayers(game){
+  document.querySelector("#total-player").innerText ="";
+  let totalPlayers = [];
+  Object.keys(game.gamePlayers).forEach(function(key){
+    totalPlayers.push(" " + game.gamePlayers[key].playerName)
+  })
+  document.querySelector("#total-player").append(totalPlayers);
+  
 }
 
 function handleFormSubmission(event)  {
@@ -146,6 +167,7 @@ function handleFormSubmission(event)  {
   game.addPlayer(newPlayer);
   // add newPlayer to game here if we make a Game() constructor
   displayGame(game,1); 
+  updateTotalPlayers(game);
   document.querySelector("input#name1").value = null;
 }
 
